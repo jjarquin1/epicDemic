d59bee69c85df10748f4d180eaff9bb53127729b
Jason Wood <jgw10713@gmail.com>
2021-06-23 17:09:57 -0700

    fixed logo path for loading screen. added fade in/out on loading screen.

--- public/js/scenes/Preload.js
+++ public/js/scenes/Preload.js
@@ -4,12 +4,21 @@ class Preload extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('logo', 'Assets/images/outbreak_logo.png')
+        this.load.image('logo', '../../images/outbreak_logo.png')
     }
 
     create() {
-        this.add.image(400, 300, 'logo')
-        this.input.on('pointerdown', () => {this.scene.start('night-phase')})
+        this.cameras.main.fadeIn(1000, 0, 0, 0)
+        this.add.image(1000, 400, 'logo')
+        this.input.on('pointerdown', () => {
+            // fade to black
+            this.cameras.main.fadeOut(1000, 0, 0, 0)
+        })
+        this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
+            this.time.delayedCall(1000, () => {
+                this.scene.start('night-phase')
+            })
+        })
     }
    
 }
