16316629af36dbb442a8f79e9fc22810654f22f4
Minh Nguyen <mnguyen728@hotmail.com>
2021-06-23 21:52:18 -0700

    added socket connection functionality

--- server.js
+++ server.js
@@ -11,8 +11,9 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const server = require('http').Server(app);
+const server = require('http').createServer(app);;
 const PORT = process.env.PORT || 3001;
+var io = require('socket.io')(server);
 
 // Set up Handlebars.js engine with custom helpers
 // const hbs = exphbs.create({ helpers });
@@ -40,6 +41,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 app.use(homeRoutes);
 app.use(gameRoutes);
+
+
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+});
 
 sequelize.sync({ force: false }).then(() => {
   server.listen(PORT, () => console.log('Now listening'));
