dd7abbe49301f7fb270938c4d012e16587720972
jkooda <77827825+jjarquin1@users.noreply.github.com>
2021-06-26 14:40:34 -0700

    changed time

--- utils/messages.js
+++ utils/messages.js
@@ -4,7 +4,7 @@ function formatMessage(username, text) {
   return {
     username,
     text,
-    time: moment().format('h:mm a')
+    time: moment.utc(fixture.kick_off).local().format('HH:mm A')
   };
 }
 
